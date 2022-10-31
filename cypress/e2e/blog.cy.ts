174951f439c7e9a84d6fade180b3070c63e79c24
Everton Vanoni Fernandes <evertonvanonifernandes@Evertons-MacBook-Pro.local>
2022-10-31 19:20:03 -0300

    before add adding

--- cypress/e2e/blog.cy.ts
+++ cypress/e2e/blog.cy.ts
@@ -3,14 +3,14 @@ import * as cypress from "cypress";
 const optionSection = 'Cypress-And-Playwright'
 
 describe('Check Vertocode blog', () => {
-
-  it(`Open the ${optionSection} section in the sections option menu.`, () => {
+  beforeEach(() => {
     cy.visit('https://blog.vertocode.com/#/')
+  })
+  it(`Open the ${optionSection} section in the sections option menu.`, () => {
     // @ts-ignore
     cy.openSection(optionSection)
   })
   it('Compare title of the Cypress VS Playwright of list with the page title.', () => {
-    cy.visit('https://blog.vertocode.com/#/')
     // @ts-ignore
     cy.openSection(optionSection)
 
@@ -20,7 +20,6 @@ describe('Check Vertocode blog', () => {
     })
   })
   it('Check element in the list by index.', () => {
-    cy.visit('https://blog.vertocode.com/#/')
     const secondItem = () => cy.get('[data-test="title-item"]').eq(1)
     secondItem().should('be.visible')
   })
